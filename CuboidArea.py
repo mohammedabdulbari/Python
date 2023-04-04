d8983fa51afd5d32d0233debe6c76fe87777843b
mohammedabdulbari <mohammed.bari@gmail.com>
2023-04-04 18:07:48 +0530

    Add files via upload

---
+++ CuboidArea.py
@@ -0,0 +1,6 @@
+length = float(input('Enter Length'))
+breadth = float(input('Enter Breadth'))
+height = float(input('Enter Height'))
+area = 2 * (length * breadth + length * height + breadth * height)
+print('Total Surface Area is', area)
+
---
+++ Rectangle.py
@@ -0,0 +1,4 @@
+length = int(input('Enter length of  Rectangle'))
+breadth = int(input('Enter breadth of a rectangle'))
+area = length * breadth
+print('Area is ', area)
