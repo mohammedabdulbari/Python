a5228ff91337dc02f90f1e10ab3ec3e52fb69c97
mohammedabdulbari <mohammed.bari@gmail.com>
2025-07-19 11:52:09 +0530

    Update and rename MatrixAdd.py to MeanMedianMode.py

---
+++ ListChallenges/MeanMedianMode.py
@@ -0,0 +1,14 @@
+
+import statistics as st
+
+lst = [6, 8, 4, 9, 3, 10, 8, 11, 5, 7, 8, 4, 2]
+
+mean = st.mean(lst)
+
+median = st.median(lst)
+
+mode = st.mode(lst)
+
+print('Mean:', mean)
+print('Median:', median)
+print('Mode:', mode)
