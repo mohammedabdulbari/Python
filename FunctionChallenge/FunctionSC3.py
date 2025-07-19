5f4cde901c000a3b6f659a0aeba043c92ff954eb
mohammedabdulbari <mohammed.bari@gmail.com>
2025-07-19 13:42:57 +0530

    Update FunctionSC3.py

--- FunctionChallenge/FunctionSC3.py
+++ FunctionChallenge/FunctionSC3.py
@@ -1,16 +1,26 @@
 
+def is_strong(password):
+    msg = 'Password must contain at least'
+    if len(password) < 8:
+        return False, msg + " 8 characters"
 
-def count(phrase):
-    lower = 0
-    upper = 0
+    has_upper = any(c.isupper() for c in password)
+    has_lower = any(c.islower() for c in password)
+    has_digit = any(c.isdigit() for c in password)
+    special_chars = set("!@#$%^&*-_+=")
+    has_special = any(c in special_chars for c in password)
 
-    for l in phrase:
-        if l.islower():
-            lower += 1
-        elif l.isupper():
-            upper = upper + 1
-    return lower, upper
+    if not has_upper:
+        return False, msg + " one uppercase letter."
+    if not has_lower:
+        return False, msg + " one lowercase letter."
+    if not has_digit:
+        return False, msg + " one digit."
+    if not has_special:
+        return False, msg + " one special char (!@#$%^&*-_+=)"
 
+    return True, "Password is strong!"
 
-print(count('ABCI'))
-
+password = input("Enter your password: ")
+valid, message = is_strong(password)
+print(message)
